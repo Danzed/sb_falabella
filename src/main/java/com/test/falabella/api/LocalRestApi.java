482aa9821fd497a6398152ad9b4b23bb40f515df
Daniel Hernandez <df.hernandez@hotmail.cl>
2020-06-17 00:41:31 -0400

    init api

--- src/main/java/com/test/falabella/api/LocalRestApi.java
+++ src/main/java/com/test/falabella/api/LocalRestApi.java
@@ -2,8 +2,9 @@ package com.test.falabella.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.apache.logging.log4j.LogManager;
@@ -15,8 +16,8 @@ import org.apache.logging.log4j.Logger;
 public class LocalRestApi {
 	private static final Logger logger = LogManager.getLogger(LocalRestApi.class);
 	
-	@GetMapping("/")
-	public ResponseEntity<?> Get() {
+	@RequestMapping(method = RequestMethod.GET, value="/{comuna}", produces={"application/json","application/xml"})
+	public ResponseEntity<?> Get(@PathVariable("comuna") String comuna) {
 		logger.info("API: getBuild");
 		return null;
 	}
